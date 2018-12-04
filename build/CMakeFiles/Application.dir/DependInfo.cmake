9c152ac21c6e4e16bf80bdf6dff5569c4f20a8e4
onome.oyibo <onome09@hotmail.com>
2018-12-04 09:16:51 +0000

    added glew and glfw

---
+++ build/CMakeFiles/Application.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/onome.oyibo/C++Stuff/gitProjects/OpenGlPractice/src/Application.cpp" "/Users/onome.oyibo/C++Stuff/gitProjects/OpenGlPractice/build/CMakeFiles/Application.dir/src/Application.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Dependencies/glfw/include"
+  "../Dependencies/glew/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Application.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Application.dir/src/Application.cpp.o"
+  "Application.pdb"
+  "Application"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Application.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
